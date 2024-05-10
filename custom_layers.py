bf27578a15e467df41b9f0acc555c3f86e7083b2
Laila H. Masoud <61348689+lailahamdy@users.noreply.github.com>
2024-05-11 00:33:51 +0300

    add custom_layers.py

--- custom_layers.py
+++ custom_layers.py
@@ -83,39 +83,25 @@ class CustomConv1DTranspose(nn.Module):
 
         return transposed_reshaped    
 
-class CustomConv1DTranspose(nn.Module):
-    def __init__(self, padding, input_channels ,filters, kernel_size, strides=1, activation='relu'):
-        super(CustomConv1DTranspose, self).__init__()
-        self.input_channels = input_channels
-        self.filters = filters
-        self.kernel_size = kernel_size
-        self.strides = strides
-        self.padding = padding
-        self.activation = activation
-
-        self.conv_transpose = nn.ConvTranspose1d(
-            in_channels=input_channels,
-            out_channels=filters,
-            kernel_size=kernel_size,
-            stride=strides,
-            padding=padding
-        )
-
-    def forward(self, inputs):
+class CustomBatchNorm1d(nn.Module):
+    def __init__(self, channels):
+        super(CustomBatchNorm1d, self).__init__()
+        self.channels = channels
         
-        inputs = inputs.contiguous()
+        self.batchnorm = nn.BatchNorm1d(channels)
 
-        # Get the input shape
-        batch_size, num_signals, channels, T = inputs.size()
+    def forward(self, x):
+        batch_size = x.size(0)
+        num_signals = x.size(1)
+        T = x.size(3)
+        
+        # Reshape x to (batch_size * num_signals, channels, T)
+        x_reshaped = x.view(-1, self.channels, T)
 
-        # Reshape the inputs to (batch_size * num_signals, channels, T)
-        inputs_reshaped = inputs.view(-1, channels, T)
+        # Apply BatchNorm1d on the reshaped tensor
+        output = self.batchnorm(x_reshaped)
 
-        # Apply the transpose convolution operation
-        conv_transposed = self.conv_transpose(inputs_reshaped)
+        # Reshape the output back to the original shape
+        output = output.view(batch_size, num_signals, -1, T)
 
-        # Reshape the transposed tensor back to (batch_size, num_signals, filters, T')
-        T_transposed = conv_transposed.size(-1)
-        transposed_reshaped = conv_transposed.view(batch_size, num_signals, self.filters, T_transposed)
-
-        return transposed_reshaped
+        return output
